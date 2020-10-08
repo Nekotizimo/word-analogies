5b12f5527c26d27bbe9dace044e5ef4aecabbf10
Nekotizimo <kennycho168@outlook.com>
2020-10-08 15:18:53 +0800

    Added Jupyter notebooks, basic analogy solving though ineffective

--- gen_data.py
+++ gen_data.py
@@ -10,17 +10,17 @@ if READVECS:
     #         if (i == 100194 - 1 or i == 148623 - 1 or i == 166545 - 1):
     #             print(line)
     #         if (i > 166545): break
-    glove_mat = np.loadtxt("glove_42B_300d.txt", usecols=(range(1, 301)), comments=None)
+    glove_mat = np.loadtxt("./data/glove_42B_300d.txt", usecols=(range(1, 301)), comments=None)
     print(glove_mat.shape)
-    np.save("./glove_mat.npy", glove_mat)
+    np.save("./data/glove_mat.npy", glove_mat)
 
 if READWORDS:
-    words = np.loadtxt("./glove_42B_300d.txt", dtype=np.str, comments=None, usecols=0)
+    words = np.loadtxt("./data/glove_42B_300d.txt", dtype=np.str, comments=None, usecols=0)
     print(words.shape)
     word_to_ind = {}
     for i, word in enumerate(words):
         word_to_ind[word] = i
     print(word_to_ind["the"])
-    with open("word_to_ind.pkl", "wb") as f:    
+    with open("./data/word_to_ind.pkl", "wb") as f:    
         pickle.dump(word_to_ind, f, pickle.HIGHEST_PROTOCOL)
-    np.save("words", words)
+    np.save("./data/words", words)
--- main.py
+++ main.py
@@ -1,8 +1,45 @@
 import numpy as np
 import pickle
 
-glove_mat = np.load("glove_mat.npya")
-with open("words_to_ind.pkl", "rb") as f:
-    words_to_ind = pickle.load(f)
+glove_mat = np.load("./data/glove_mat.npy")
+# words = np.load("./data/words.npy")
+with open("./data/word_to_ind.pkl", "rb") as f:
+    word_to_ind = pickle.load(f)
 
+given_f, given_s, ques_f = "father", "mother", "brother"
+given_s_opts = ["grandmother", "grandfather", "daughter", "sister", "mother"]
 
+given_f_v = glove_mat[word_to_ind[given_f]]
+given_s_v = glove_mat[word_to_ind[given_s]]
+ques_f_v = glove_mat[word_to_ind[ques_f]]
+
+given_diff = given_s_v - given_f_v
+
+max_sim, max_i = 0, 0
+
+# for i, opt in enumerate(given_s_opts):
+#     opt_v = glove_mat[word_to_ind[opt]]
+#     diff_vec = opt_v - ques_f_v
+#     sim = given_diff @ diff_vec / np.linalg.norm(given_diff) / np.linalg.norm(diff_vec)
+#     print(opt, round(sim, 4))
+#     if (sim > max_sim): max_sim, max_i = sim, i
+
+diff_mat = glove_mat - ques_f_v
+diff_mat_norms = np.linalg.norm(diff_mat, axis=1, keepdims=True)
+print(diff_mat.shape, diff_mat_norms.shape)
+
+given_diff = np.reshape(given_diff, (-1, 1)) # to col vec
+given_diff_norm = np.linalg.norm(given_diff, keepdims=True)
+print(given_diff.shape, given_diff_norm.shape)
+
+diff_mat_sims = diff_mat @ given_diff
+print(diff_mat_sims.shape)
+diff_mat_sims = diff_mat_sims / diff_mat_norms / given_diff_norm
+print(diff_mat_sims.shape)
+
+max_sim = np.amax(diff_mat_sims)
+max_i = np.argmax(diff_mat_sims)
+
+print("Prediction:", given_s_opts[max_i], round(max_sim, 4))
+
+    
